6d66af0833be0174c20e76636590cce629989a91
Wesley <weslcld@gmail.com>
2020-06-14 23:42:32 -0300

    Updates

---
+++ precache-manifest.9ba52160bff29dda6e4ef495359d2a5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1bcd5ef43d630c5f5508da21dd7df31",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0d9b22520b7ae2ac69f6",
+    "url": "/portfolio/static/css/2.9e5c3921.chunk.css"
+  },
+  {
+    "revision": "6a7f73816dcc89075b11",
+    "url": "/portfolio/static/css/main.d15b5899.chunk.css"
+  },
+  {
+    "revision": "0d9b22520b7ae2ac69f6",
+    "url": "/portfolio/static/js/2.177dab01.chunk.js"
+  },
+  {
+    "revision": "3a5402f5ddd536a8f5deb8d906cf7b41",
+    "url": "/portfolio/static/js/2.177dab01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a7f73816dcc89075b11",
+    "url": "/portfolio/static/js/main.5c6060e3.chunk.js"
+  },
+  {
+    "revision": "4de93b1f99bf6174da2b",
+    "url": "/portfolio/static/js/runtime-main.c66d8078.js"
+  }
+]);
